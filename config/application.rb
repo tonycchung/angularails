00c833bc1e928df3252d695c81305a86c8c7447a
Tony Chung <anthonyuchung@gmail.com>
2014-10-31 14:20:14 -0700

    Add gems. Add bootstrap and angular.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Angularails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.stylesheets false
+      g.helper false
+      g.javascripts false
+      g.test_framework false
+    end
   end
 end
